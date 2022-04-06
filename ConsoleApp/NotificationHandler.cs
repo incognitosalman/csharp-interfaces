2b21d19b40492a9f753c9b5e242280914a53152c
Administrator <Administrator@TRAINING>
2022-04-06 10:31:50 +0500

    Add project files.

---
+++ ConsoleApp/NotificationHandler.cs
@@ -0,0 +1,30 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class NotificationHandler
+    {
+        private readonly INotification[] notifications;
+        private int index = 0;
+        public NotificationHandler(int size)
+        {
+            notifications = new INotification[size];
+        }
+
+        public void Add(INotification notification)
+        {
+            notifications[index] = notification;
+            index++;
+        }
+
+        public INotification[] GetNotifications()
+        {
+            return notifications;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Models;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            NotificationHandler notificationHandler = new NotificationHandler(100);
+
+            notificationHandler.Add(new EmailNotification());
+            notificationHandler.Add(new SmsNotification());
+
+
+            var notifications = notificationHandler.GetNotifications();
+
+            for (int i = 0; i < notifications.Length; i++)
+            {
+                if (notifications[i] != null)
+                    notifications[i].Notify($"Hello {i + 1}");
+            }
+        }
+    }
+}
